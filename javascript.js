1690e1370c8b70d6e4aa6a39549b1f844923fdc3
Ebrahim Badawy <121634763+Ebrahim-N4@users.noreply.github.com>
2026-05-16 14:48:11 +0300

    Delete javascript.js
    delete old JS

--- javascript.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
-  if(form){
-    form.addEventListener("submit", e => {
-      e.preventDefault();
-      alert("Your message has been sent successfully!");
-    });
-  }
-});
